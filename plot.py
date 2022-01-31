4b91e97b34d5ba3e2adaee2f9ca3b2fb3961e114
Tiziano Zito <opossumnano@gmail.com>
2022-01-31 11:35:41 +0100

    remove old commented out code

--- plot.py
+++ plot.py
@@ -15,20 +15,14 @@ def plot_account_balance(years, sums, diffs):
     gs = gridspec.GridSpec(2, 1)
 
     ax1 = plt.subplot(gs[0])
-    #ax1.plot(years, sums, '*')
     ax1.fill_between(years, sums, color='olive')
     ax1.grid(True)
     ax1.xaxis.set_major_locator(MultipleLocator(1))
     ax1.xaxis.tick_top()
     ax1.xaxis.set_label_position('top')
-    #plt.setp(ax1.get_xticklabels(), visible=False)
-    #ax2.plot(years, diffs, '*')
-
 
     ax2 = plt.subplot(gs[1], sharex = ax1)
     ax2.fill_between(years, diffs, color='darkkhaki')
-    #ax2.grid(True)
-    #ax2.xaxis.set_major_locator(MultipleLocator(1))
     plt.subplots_adjust(hspace=.01)
     plt.show()
 
