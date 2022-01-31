da40cb068f0444e7a478d6d4c7214b2b056434e3
Tiziano Zito <opossumnano@gmail.com>
2022-01-31 12:00:01 +0100

    move show outside of the function and refactor it

--- plot.py
+++ plot.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from matplotlib import gridspec
 from matplotlib.ticker import MultipleLocator
 
+
 data = np.loadtxt('stats.csv', delimiter=',')
 years = data[:,0]
 sums = data[:,1]/1000
@@ -11,20 +12,20 @@ diffs = data[:,2]/1000
 plt.style.use('ggplot')
 
 def plot_account_balance(years, sums, diffs):
-    fig = plt.figure()
-    gs = gridspec.GridSpec(2, 1)
+    fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
 
-    ax1 = plt.subplot(gs[0])
     ax1.fill_between(years, sums, color='olive')
     ax1.grid(True)
     ax1.xaxis.set_major_locator(MultipleLocator(1))
-    ax1.xaxis.tick_top()
-    ax1.xaxis.set_label_position('top')
+    ax1.tick_params(axis='x', top=True, labeltop=True)
+    ax1.tick_params(axis='y', right=True, labelright=True)
 
-    ax2 = plt.subplot(gs[1], sharex = ax1)
     ax2.fill_between(years, diffs, color='darkkhaki')
-    plt.subplots_adjust(hspace=.01)
-    plt.show()
+    ax2.tick_params(axis='y', right=True, labelright=True)
 
-plot_account_balance(years, sums, diffs)
+    fig.subplots_adjust(hspace=.01)
+    return fig
 
+fig = plot_account_balance(years, sums, diffs)
+
+plt.show()
