0d36e309713d0a1082ad330e36d8b322b01b3b4d
Tiziano Zito <opossumnano@gmail.com>
2022-02-10 12:10:42 +0100

    moved the bulk of the plotting code to a function

--- plot.py
+++ plot.py
@@ -10,19 +10,21 @@ diffs = data[:,2]/1000
 
 plt.style.use('ggplot')
 
-fig = plt.figure()
-gs = gridspec.GridSpec(2, 1)
+def plot_stuff(years, sums, diffs):
+    fig = plt.figure()
+    gs = gridspec.GridSpec(2, 1)
 
-ax1 = plt.subplot(gs[0])
-ax1.fill_between(years, sums, color='olive')
-ax1.grid(True)
-ax1.xaxis.set_major_locator(MultipleLocator(1))
-ax1.xaxis.tick_top()
-ax1.xaxis.set_label_position('top')
+    ax1 = plt.subplot(gs[0])
+    ax1.fill_between(years, sums, color='olive')
+    ax1.grid(True)
+    ax1.xaxis.set_major_locator(MultipleLocator(1))
+    ax1.xaxis.tick_top()
+    ax1.xaxis.set_label_position('top')
 
 
-ax2 = plt.subplot(gs[1], sharex = ax1)
-ax2.fill_between(years, diffs, color='darkkhaki')
-plt.subplots_adjust(hspace=.01)
-plt.show()
+    ax2 = plt.subplot(gs[1], sharex = ax1)
+    ax2.fill_between(years, diffs, color='darkkhaki')
+    plt.subplots_adjust(hspace=.01)
+    plt.show()
 
+plot_stuff(years, sums, diffs)
